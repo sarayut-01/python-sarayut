42b2ab4abfdf23a94fe0df1ce16033601f78343f
sarayut-01 <sarayut.ap@rmuti.ac.th>
2024-06-27 11:34:27 +0700

    first commit

---
+++ world.py
@@ -0,0 +1,76 @@
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
+x1 = x2=10
+x1&=3
+print (x1)
+print (x2)
+
