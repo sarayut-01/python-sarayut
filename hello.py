deb2915fe158ab282f1809e3be90961ce3faf18e
sarayut-01 <sarayut.ap@rmuti.ac.th>
2024-07-04 12:08:53 +0700

    เพิ่มใหม่week3

--- hello.py
+++ hello.py
@@ -1,29 +1,34 @@
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')
-print ('hello worlds hd')print ('hello worlds hd')print ('hello worlds hd')print ('hello worlds hd')
+#i = 1
+
+#while i <= 100:
+#    if i % 2 == 1 :
+#        print(i)
+#        i+=2
+#fruits = ["kla","mos","golf","peac","not"]
+#for x in fruits:
+#    print(x, end = '         ' )
+
+
+
+
+def b (num,num1) :
+    print (num+num1)
+
+def a (num,num1):
+    print (num-num1)
+def c (num,num1):
+    print (num/num1)
+def d (num,num1):
+    print (num*num1)
+def m (num,num1):
+    print (num%num1)
+def n (num,num1):
+    print (num**num1)
+
+
+b (30,40)    
+a (50,40)
+c (55,5)
+d (2,6)
+m (3,11)
+n (2,7)
